5c7fd7c2ce149030669c37ec2556c1ae4db34b02
Горячев Никита ТКИ-141 <nicknamexdd@yandex.kz>
2026-03-31 10:45:09 +0300

    Create point.h

---
+++ point.h
@@ -0,0 +1,49 @@
+#pragma once
+
+/**
+ * @brief Класс Точка
+ */
+class Point
+{
+private:
+    /**
+     * @brief Координата X
+     */
+    double x;
+
+    /**
+     * @brief Координата Y
+     */
+    double y;
+
+public:
+    /**
+     * @brief Конструктор точки
+     */
+    Point(double x = 0, double y = 0);
+
+    /**
+     * @brief Получение координаты X
+     */
+    double getX() const;
+
+    /**
+     * @brief Получение координаты Y
+     */
+    double getY() const;
+
+    /**
+     * @brief Проверка на равенство оператора
+     */
+    bool operator==(Point p) const;
+
+    /**
+     * @brief Проверка на неравенство оператора
+     */
+    bool operator!=(Point p) const;
+
+    /**
+     * @brief Расстояние между двумя точками
+     */
+    double distanceTo(const Point& other) const;
+};
