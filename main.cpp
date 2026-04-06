03cfe87c3633a29c1f39af6a9c9ac5c96be55032
Горячев Никита ТКИ-141 <nicknamexdd@yandex.kz>
2026-04-06 23:09:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,23 +15,21 @@ double getValue(std::string report = "");
  * @brief Точка входа в программу
  * @return 0, если программа выполнена корректно
  */
+
+Point getPoint();
+
+/**
+ * @brief Точка входа в программу
+ * @return 0, если программа выполнена корректно
+ */
 int main()
 {
     std::cout << "Enter the coordinates of the four vertices of the rectangle: " << std::endl;
 
-    double x1 = getValue("x1: ");
-    double y1 = getValue("y1: ");
-    double x2 = getValue("x2: ");
-    double y2 = getValue("y2: ");
-    double x3 = getValue("x3: ");
-    double y3 = getValue("y3: ");
-    double x4 = getValue("x4: ");
-    double y4 = getValue("y4: ");
-
-    Point p1(x1, y1);
-    Point p2(x2, y2);
-    Point p3(x3, y3);
-    Point p4(x4, y4);
+    Point p1 = getPoint();
+    Point p2 = getPoint();
+    Point p3 = getPoint();
+    Point p4 = getPoint();
 
     Rectangle myRectangle(p1, p2, p3, p4);
 
@@ -53,4 +51,11 @@ double getValue(std::string report)
         exit(1);
     }
     return value;
+}
+
+Point getPoint()
+{
+    double x = getValue("x: ");
+    double y = getValue("y: ");
+    return Point(x,y);
 }
